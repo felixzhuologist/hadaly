729aa74468b5a308d0c9e2334190591cdb030638
felixzhuologist <felix.czhu@gmail.com>
2016-10-20 21:44:28 -0400

    Add chrome devtools

--- static/js/configureStore.js
+++ static/js/configureStore.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
@@ -6,13 +7,18 @@ import rootReducer from './reducers/rootReducer';
 
 const configureStore = () => {
   const middlewares = [thunk];
-  if (process.env.NODE_ENV !== 'prod') {
+  if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger());
   }
 
+  const composeEnhancers = composeWithDevTools({
+    // Specify here name, actionsBlacklist, actionsCreators and other options
+    shouldCatchErrors: true,
+  });
+
   return createStore(
     rootReducer,
-    applyMiddleware(...middlewares)
+    composeEnhancers(applyMiddleware(...middlewares))
   );
 };
 
