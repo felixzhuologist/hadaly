65dd84c192f97f308a43368b6e76f95b4c718a03
felixzhuologist <felix.czhu@gmail.com>
2016-10-21 17:49:59 -0400

    Try and setup HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
     './static/js/app.js'
   ],
   output: {
@@ -19,4 +23,7 @@ module.exports = {
   eslint: {
     configFile: '.eslintrc',
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
