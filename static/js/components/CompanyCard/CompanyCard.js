e0ba0267e81db61a674572d56b0909a57cbdc4e9
felixzhuologist <felix.czhu@gmail.com>
2016-11-04 19:19:15 -0400

    Disable apply when nourl

--- static/js/components/CompanyCard/CompanyCard.js
+++ static/js/components/CompanyCard/CompanyCard.js
@@ -48,7 +48,8 @@ class CompanyCard extends React.Component {
 
     const cardFooter = this.state.stages.length == 0 ?
       <CardActions>
-        <FlatButton label="apply" 
+        <FlatButton label="apply"
+                    disabled={this.state.url === ""}
                     onTouchTap={() => window.open(this.state.url, '_blank')} />
         <FlatButton label="update" onTouchTap={() => console.log("")} />
       </CardActions>
